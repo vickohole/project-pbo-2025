e60da65b2b679a266bf90b34c5c66e9e1634d492
vickohole <vickohole@github.com>
2025-07-19 02:09:51 +0700

    🔥 fresh from the oven

--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -3,6 +3,4 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\PaymentPanelProvider::class,
-    App\Providers\Filament\ProductPanelProvider::class,
 ];
